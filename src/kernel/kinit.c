875cb67bdfbcf11f3d28a5f407d38861dd1d2d41
frogrammer9 <oskar@meler.pl>
2025-03-30 20:03:37 +0200

    worked on page table interface

--- src/kernel/kinit.c
+++ src/kernel/kinit.c
@@ -22,9 +22,10 @@
  */
 
 // NOTE: This is an example so far.
-[[noreturn]] void kinit(u64 kernel_physical_address, u64 kernel_size, u64 stack_physical_address, u64 stack_size) {
+[[noreturn]] void kinit(u64 kernel_physical_address, u64 kernel_size, u64 stack_physical_address, u64 stack_size, void* ram_start) {
 	DEBUG_PRINTF("Kernel loaded at phisical address: 0x%lx (size: %luMB). Stack at: 0x%lx (size: %luMB)\n", kernel_physical_address, kernel_size >> 20, stack_physical_address, stack_size >> 20);
-	error_t err = virtual_memory_init(VMS_BARE, 0);
+	error_t err = virtual_memory_init(ram_start);
+	err = virtual_memory_enable(VMS_BARE, 0);
 	if(err)
 		DEBUG_PRINTF("Error: %s\n", get_error_msg(err));
 	else
