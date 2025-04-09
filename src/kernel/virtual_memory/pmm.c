7ed3749202a13dd623f75cd746d0f7aa537a62ca
frogrammer9 <oskar@meler.pl>
2025-04-09 20:06:55 +0200

    implemented basic page table managment

--- src/kernel/virtual_memory/pmm.c
+++ src/kernel/virtual_memory/pmm.c
@@ -1,7 +1,7 @@
 #include "pmm.h"
 
 error_t alloc_frame(page_size_t psize, physical_page_number_t* ppnOUT) {
-	return ERR_NONE;
+	return ERR_NOT_IMPLEMENTED;
 }
 
-void free_frame() {}
+void free_frame(physical_page_number_t ppn) {}
--- src/kernel/virtual_memory/pmm.h
+++ src/kernel/virtual_memory/pmm.h
@@ -7,6 +7,7 @@
 #include "mm_common.h"
 
 [[nodiscard]] error_t alloc_frame(page_size_t psize, physical_page_number_t* ppnOUT);
-void free_frame();
+void pmm_set_memory_regions_as_reserved(); //TODO: Implement (it doesn't have to be called this, this is just an information that such function needs to be implemented)
+void free_frame(physical_page_number_t ppn);
 
 #endif //!_KERNEL_VIRTUAL_MEMORY_PMM_H_
