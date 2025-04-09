f8dc20f4d3359dd668779f3d6a678d0da0931dff
frogrammer9 <oskar@meler.pl>
2025-04-09 20:10:37 +0200

    fixed compiler error and formation, changed kinit arguments

--- src/kernel/virtual_memory/mm_common.c
+++ src/kernel/virtual_memory/mm_common.c
@@ -2,8 +2,6 @@
 
 static virt_addr_t g_RAM_start = nullptr;
 
-static virt_mem_scheme_t g_active_mem_scheme = VMS_BARE;
-
 virt_addr_t physical_to_virtual(phys_addr_t paddr) {
 	return g_RAM_start + paddr;
 }
