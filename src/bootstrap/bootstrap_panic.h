25379fbf16aa1030e6025f2f96e1bc1078873b5f
Kamil Zdancewicz <kazdancewicz@gmail.com>
2025-05-26 22:13:00 +0200

    Fixed the basic functioncs of the DT
    Basic device tree operations work, as shown with the example uart search

--- src/bootstrap/bootstrap_panic.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BOOTSTRAP_BOOTSTRAP__PANIC_H__
-#define __BOOTSTRAP_BOOTSTRAP__PANIC_H__
-
-#include <stdbigos/types.h>
-
-#define PANIC(msg) bootstrap_panic(__FILE__, __LINE__, msg)
-[[noreturn]] void bootstrap_panic(const char* file_name, u64 line, const char* msg);
-
-#endif // !__BOOTSTRAP_BOOTSTRAP_PANIC_H__
