55a2dcaa4e477bb66251ad82f0cf7510c7cf6412
Oskar Meler <oskar@meler.pl>
2025-06-06 21:34:33 +0200

    added kernel headers

---
+++ src/kernel/kinit.c
@@ -0,0 +1,7 @@
+#include "memory_managment/physical_memory_manager.h"
+#include "memory_managment/address_space_manager.h"
+#include "kernel_config.h"
+
+[[noreturn]] void kinit(phys_addr_t device_tree) {
+
+}
