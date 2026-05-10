4c2427d90b9f48aa8b582af0c831e9765614edfd
Krzychuo <krzychuo93@gmail.com>
2026-05-11 00:07:58 +0200

    Applied changes suggested in #103 pull request by frogrammer9

--- include/stdbigos/string_view.h
+++ include/stdbigos/string_view.h
@@ -2,36 +2,21 @@
 #define STDBIGOS_STRING_VIEW
 
 #include <stddef.h>
+#include <stdbigos/math.h>
+#include <stdbigos/string.h>
 
 typedef struct {
 	const char* data;
 	size_t count;
 } string_view;
 
-static inline string_view strv_init(const char* str) {
+static inline string_view string_view_init(const char* str) {
 	if (!str)
-		return (string_view){NULL, 0};
-	string_view ret;
-	ret.data = str;
-
-	const char* i = str;
-	while (*i) {
-		i++;
-	}
-
-	ret.count = (size_t)(i - str);
-	return ret;
+		return (string_view){nullptr, 0};
+	return (string_view){str, strlen(str)};
 }
 
-static inline size_t strvlen(string_view strv) {
-	return strv.count;
-}
-
-static inline size_t strvnlen(string_view strv, size_t maxlen) {
-	return strv.count < maxlen ? strv.count : maxlen;
-}
-
-static inline string_view strv_pop_front(string_view strv) {
+static inline string_view string_view_pop_front(string_view strv) {
 	if (strv.count == 0)
 		return strv;
 	strv.data++;
@@ -39,15 +24,15 @@ static inline string_view strv_pop_front(string_view strv) {
 	return strv;
 }
 
-static inline string_view strv_pop_back(string_view strv) {
+static inline string_view string_view_pop_back(string_view strv) {
 	if (strv.count == 0)
 		return strv;
 	strv.count--;
 	return strv;
 }
 
-static inline int strvcmp(string_view a, string_view b) {
-	size_t min = a.count < b.count ? a.count : b.count;
+static inline int string_view_cmp(string_view a, string_view b) {
+	size_t min = MIN(a.count, b.count);
 
 	for (size_t i = 0; i < min; i++) {
 		unsigned char ca = (unsigned char)a.data[i];
@@ -66,10 +51,8 @@ static inline int strvcmp(string_view a, string_view b) {
 	return 0;
 }
 
-static inline int strvncmp(string_view a, string_view b, size_t n) {
-	size_t min = a.count < b.count ? a.count : b.count;
-	if (min > n)
-		min = n;
+static inline int string_view_ncmp(string_view a, string_view b, size_t n) {
+	size_t min = MIN(MIN(a.count, b.count), n);
 
 	for (size_t i = 0; i < min; i++) {
 		unsigned char ca = (unsigned char)a.data[i];
@@ -88,59 +71,59 @@ static inline int strvncmp(string_view a, string_view b, size_t n) {
 	return 0;
 }
 
-static inline string_view strvchr(string_view strv, int c) {
+static inline string_view string_view_chr(string_view strv, int c) {
 	if (!strv.data)
 		return (string_view){NULL, 0};
 	unsigned char ch = (unsigned char)c;
 	while (strv.count > 0 && (unsigned char)strv.data[0] != ch) {
-		strv = strv_pop_front(strv);
+		strv = string_view_pop_front(strv);
 	}
 	return strv;
 }
 
-static inline string_view strvtok(string_view strv, string_view delim) {
+static inline string_view string_view_tokenize(string_view strv, string_view delim) {
 	if (!strv.data)
 		return (string_view){NULL, 0};
 	string_view ret;
 	ret.data = strv.data;
 	ret.count = 0;
-	while (strv.count > 0 && strvchr(delim, *strv.data).count == 0) {
-		strv = strv_pop_front(strv);
+	while (strv.count > 0 && string_view_chr(delim, *strv.data).count == 0) {
+		strv = string_view_pop_front(strv);
 		ret.count++;
 	}
 	return ret;
 }
 
-static inline size_t strvspn(string_view strv, string_view chars) {
+static inline size_t string_view_spn(string_view strv, string_view chars) {
 	if (!strv.data)
 		return 0;
 	for (size_t i = 0; i < strv.count; i++) {
-		if (strvchr(chars, strv.data[i]).count == 0)
+		if (string_view_chr(chars, strv.data[i]).count == 0)
 			return i;
 	}
 	return strv.count;
 }
 
-static inline size_t strvcspn(string_view strv, string_view chars) {
+static inline size_t string_view_cspn(string_view strv, string_view chars) {
 	if (!strv.data)
 		return 0;
 	for (size_t i = 0; i < strv.count; i++) {
-		if (strvchr(chars, strv.data[i]).count > 0)
+		if (string_view_chr(chars, strv.data[i]).count > 0)
 			return i;
 	}
 	return strv.count;
 }
 
-static inline string_view strvpbrk(string_view strv, string_view breakset) {
+static inline string_view string_view_pbrk(string_view strv, string_view breakset) {
 	if (!strv.data)
 		return (string_view){NULL, 0};
-	while (strv.count > 0 && strvchr(breakset, strv.data[0]).count == 0) {
-		strv = strv_pop_front(strv);
+	while (strv.count > 0 && string_view_chr(breakset, strv.data[0]).count == 0) {
+		strv = string_view_pop_front(strv);
 	}
 	return strv;
 }
 
-static inline string_view strvstrv(string_view strv, string_view substrv) {
+static inline string_view string_view_str(string_view strv, string_view substrv) {
 	if (!strv.data)
 		return (string_view){NULL, 0};
 	if (substrv.count == 0)
@@ -150,7 +133,7 @@ static inline string_view strvstrv(string_view strv, string_view substrv) {
 	size_t count = strv.count;
 	strv.count = substrv.count;
 	for (size_t i = 0; i <= count - substrv.count; i++) {
-		if (strvcmp(strv, substrv) == 0)
+		if (string_view_cmp(strv, substrv) == 0)
 			return strv;
 		strv.data++;
 	}
