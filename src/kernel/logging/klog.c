05cea5a8f4919012037ee392a5e3a2e90919066f
Krzychuo <krzychuo93@gmail.com>
2026-05-10 23:42:01 +0200

    Applied changes suggested in #92 pull request by frogrammer9

--- src/kernel/logging/klog.c
+++ src/kernel/logging/klog.c
@@ -7,16 +7,16 @@
 #include "debug/debug_stdio.h"
 
 static u32 g_indent_level = 0;
-static const char* g_prefixes[] = {"[ERROR]", "[WARNING]", "[ ]", "[~]"};
-static const char* g_overflow_warning = "[WARNING]Some of the logs were lost due to overflow";
+static const char* g_prefixes[] = {"[ERROR] ", "[WARNING] ", "[ ] ", "[~] "};
+static const char* g_overflow_warning = "[WARNING] Some of the logs were lost due to overflow";
 
 #ifdef __DEBUG__
-	#define RING_BUF_SIZE (1 << 20)
+const size_t RING_BUF_SIZE = 1 << 20;
 #else
-	#define RING_BUF_SIZE (1 << 16)
+const size_t RING_BUF_SIZE = 1 << 16;
 #endif // !__DEBUG__
 
-#define TEMP_BUF_SIZE (1 << 12)
+const size_t TEMP_BUF_SIZE = 1 << 12;
 
 typedef struct {
 	char ring[RING_BUF_SIZE];
@@ -24,27 +24,23 @@ typedef struct {
 	i32 write;
 	bool full;
 	bool overflown;
-	void (*uart_tx)(char c);
+	void (*serial_tx)(char c);
 } ring_buffer_t;
 
 static ring_buffer_t g_buffer;
 
-static ring_buffer_t* get_buffer() {
-	return &g_buffer;
-}
-
 static void put_char(ring_buffer_t* rb, const char c) {
 	if (c == '\0')
-		return; // Null characters are not suppossed to be logged.
-	rb->ring[rb->write++] = c;
+		return;                                    // Null characters are not suppossed to be logged.
+	rb->ring[rb->write++] = (c == '\t' ? ' ' : c); // '\t' char is apparently wasteful
 	rb->write %= RING_BUF_SIZE;
 	if (rb->full) {
 		rb->read = (rb->read + 1) % RING_BUF_SIZE;
 		rb->overflown = true;
 	}
 	rb->full = rb->read == rb->write;
-	if (rb->uart_tx)
-		rb->uart_tx(c);
+	if (rb->serial_tx)
+		rb->serial_tx(c);
 }
 
 static void put_string(ring_buffer_t* rb, const char* str) {
@@ -52,45 +48,34 @@ static void put_string(ring_buffer_t* rb, const char* str) {
 	while (str[it]) put_char(rb, str[it++]);
 }
 
-static void put_overflow_warning(ring_buffer_t* rb) {
+static void put_overflow_warning() {
 	size_t it = 0;
-	i32 temp_write = rb->write;
-	while (g_overflow_warning[it]) {
-		rb->ring[temp_write++] = g_overflow_warning[it++];
-		temp_write %= RING_BUF_SIZE;
-	}
+	while (g_overflow_warning[it]) g_buffer.serial_tx(g_overflow_warning[it++]);
 }
 
-static void put_msg(ring_buffer_t* rb, const char* msg) {
-	put_string(rb, msg);
-	if (rb->overflown)
-		put_overflow_warning(rb);
-}
-
-char ring_buffer_get_char() {
-	ring_buffer_t* rb = get_buffer();
-	if (rb->read == rb->write && !rb->full)
+static char ring_buffer_get_char() {
+	if (g_buffer.read == g_buffer.write && !g_buffer.full)
 		return 0;
-	const char ret = rb->ring[rb->read++];
-	rb->read %= RING_BUF_SIZE;
-	rb->full = false;
-	rb->overflown = false;
+	const char ret = g_buffer.ring[g_buffer.read++];
+	g_buffer.read %= RING_BUF_SIZE;
+	g_buffer.full = false;
+	g_buffer.overflown = false;
 	return ret;
 }
 
-void ring_buffer_flush_to_uart() {
-	ring_buffer_t* rb = get_buffer();
-	if (!rb->uart_tx)
+void klog_flush_serial() {
+	if (!g_buffer.serial_tx)
 		return;
+	if (g_buffer.overflown)
+		put_overflow_warning();
 	char c;
-	while ((c = ring_buffer_get_char())) rb->uart_tx(c);
+	while ((c = ring_buffer_get_char())) g_buffer.serial_tx(c);
 }
 
-void ring_buffer_set_uart_tx_function(void (*uart_tx)(char c), bool flush) {
-	ring_buffer_t* rb = get_buffer();
-	rb->uart_tx = uart_tx;
+void klog_set_serial_handler(void (*serial_tx)(char c), bool flush) {
+	g_buffer.serial_tx = serial_tx;
 	if (flush)
-		ring_buffer_flush_to_uart();
+		klog_flush_serial();
 }
 
 void klog_indent_increase() {
@@ -111,7 +96,8 @@ static void klogv(klog_severity_level_t loglvl, const char* fmt, va_list va) {
 	}
 
 	size_t written = 0;
-	for (u32 i = 0; i < g_indent_level && written < TEMP_BUF_SIZE - 1; i++) temp_buffer[written++] = ' ';
+	for (u32 i = 0; i < g_indent_level && written < TEMP_BUF_SIZE - 1; i++)
+		temp_buffer[written++] = (char)(g_indent_level > 8 ? 8 : g_indent_level);
 
 	const char* prefix = g_prefixes[loglvl];
 	for (size_t i = 0; prefix[i] && written < TEMP_BUF_SIZE - 1; i++) temp_buffer[written++] = prefix[i];
@@ -130,18 +116,12 @@ static void klogv(klog_severity_level_t loglvl, const char* fmt, va_list va) {
 		written = TEMP_BUF_SIZE - 1;
 	temp_buffer[written] = '\0';
 
-	ring_buffer_t* rb = get_buffer();
-	put_msg(rb, temp_buffer);
-
-	// DEBUG_PUTGAP(g_indent_level);
-	// DEBUG_PRINTF("%s ", g_prefixes[loglvl]);
-	// DEBUG_VPRINTF(fmt, va);
+	put_string(&g_buffer, temp_buffer);
 }
 
 static void kloglnv(klog_severity_level_t loglvl, const char* fmt, va_list va) {
 	klogv(loglvl, fmt, va);
-	put_msg(&g_buffer, "\n");
-	// DEBUG_PUTC('\n');
+	put_string(&g_buffer, "\n");
 }
 
 void klog(klog_severity_level_t loglvl, const char* fmt, ...) {
